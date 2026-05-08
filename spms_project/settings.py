4f448778c7d77d0b9791ce5d29a0e543a36cad11
bachuakshay123 <BACHU.AKSHAY123@GMAIL.COM>
2026-05-08 16:55:06 +0530

    2nd

--- spms_project/settings.py
+++ spms_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'spms_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spms',
+        'PASSWORD':'Akshay@1234',
+        'USER':'root',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
