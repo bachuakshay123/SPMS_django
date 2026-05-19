9884ac83046fd080d0373f0a3e7188fe5de3cdda
bachuakshay123 <BACHU.AKSHAY123@GMAIL.COM>
2026-05-19 16:32:13 +0530

    created Models

--- spms/admin.py
+++ spms/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from spms.models import login,Category,Vehicle
+admin.site.register(login)
+admin.site.register(Category)
+admin.site.register(Vehicle)
--- spms/models.py
+++ spms/models.py
@@ -5,3 +5,35 @@ class login (models.Model):
     mobile_number=models.CharField(max_length=10, unique=True)
     def __str__(self):
         return self.username
+    
+
+class Category(models.Model):
+    parking_area_number=models.IntegerField()
+    vehicle_type=models.CharField(max_length=100)
+    vehicle_limit=models.IntegerField()
+    parking_charge=models.DecimalField(max_digits=10,decimal_places=2)
+    status=models.BooleanField(default=True)
+    def __str__(self):
+        return self.vehicle_type
+    
+
+class Vehicle(models.Model):
+    STATUS_CHOICES = (
+        ('Parked', 'Parked'),
+        ('Leaved', 'Leaved'),
+    )
+    PAYMENT_CHOICES = (
+        ('Paid', 'Paid'),
+        ('Pending', 'Pending'),
+    )
+    vehicle_number=models.CharField(max_length=50)
+    vehicle_type=models.ForeignKey(Category, on_delete=models.CASCADE)
+    area_number=models.IntegerField()
+    parking_charge=models.DecimalField(max_digits=10,decimal_places=2)
+    arrival_time=models.DateTimeField(auto_now_add=True)
+    departure_time=models.DateTimeField(null=True,blank=True)
+    parking_status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='Parked')
+    payment_status=models.CharField(max_length=20,choices=PAYMENT_CHOICES,default='Pending')
+    wrong_parking=models.BooleanField(default=False)
+    def __str__(self):
+        return self.vehicle_number
